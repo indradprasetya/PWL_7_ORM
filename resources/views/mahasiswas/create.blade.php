ab14be60baa66acd5a472b67be52b8e9c3cca643
indradprasetya <indrayanadian7@gmail.com>
2023-06-18 20:56:56 +0700

    fix

--- resources/views/mahasiswas/create.blade.php
+++ resources/views/mahasiswas/create.blade.php
@@ -22,16 +22,28 @@
                                 class="form-control" id="Nama" aria-describedby="Nama"> </div>
                         <div class="form-group"> <label for="Kelas">Kelas</label>
                             <input type="Kelas" name="Kelas" class="form-control" id="Kelas"
-                                aria-describedby="password"><div class="card-body"> <ul class="list-group list-group-flush"> <li class="list-group-item"><b>Nim: </b>{{$Mahasiswa->Nim}}</li> <li class="list-group-item"><b>Nama: </b>{{$Mahasiswa->Nama}}</li> <li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa->Kelas}}</li> <li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa->Jurusan}}</li> <li class="list-group-item"><b>No_Handphone: </b>{{$Mahasiswa->No_Handphone}}</li> </ul> </div> <a class="btn btn-success mt-3" href="{{ route('mahasiswas.index') }}">Kembali</a> </div> </div> </div> @endsection
+                                aria-describedby="password">
+                            <div class="card-body">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item"><b>Nim: </b>{{ $Mahasiswa->Nim }}</li>
+                                    <li class="list-group-item"><b>Nama: </b>{{ $Mahasiswa->Nama }}</li>
+                                    <li class="list-group-item"><b>Kelas: </b>{{ $Mahasiswa->Kelas }}</li>
+                                    <li class="list-group-item"><b>Jurusan: </b>{{ $Mahasiswa->Jurusan }}</li>
+                                    <li class="list-group-item"><b>No_Handphone: </b>{{ $Mahasiswa->No_Handphone }}</li>
+                                </ul>
+                            </div> <a class="btn btn-success mt-3" href="{{ route('mahasiswas.index') }}">Kembali</a>
                         </div>
-                        <div class="form-group"> <label for="Jurusan">Jurusan</label> <input type="Jurusan" name="Jurusan"
-                                class="form-control" id="Jurusan" aria-describedby="Jurusan"> </div>
-                        <div class="form-group"> <label for="No_Handphone">No_Handphone</label> <input type="No_Handphone"
-                                name="No_Handphone" class="form-control" id="No_Handphone" aria-describedby="No_Handphone">
-                        </div> <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
                 </div>
-            </div>
-        </div>
+        </div> @endsection
     </div>
+    <div class="form-group"> <label for="Jurusan">Jurusan</label> <input type="Jurusan" name="Jurusan"
+            class="form-control" id="Jurusan" aria-describedby="Jurusan"> </div>
+    <div class="form-group"> <label for="No_Handphone">No_Handphone</label> <input type="No_Handphone"
+            name="No_Handphone" class="form-control" id="No_Handphone" aria-describedby="No_Handphone">
+    </div> <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+</div>
+</div>
+</div>
 @endsection
